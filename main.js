4705fb9dedc38fa3a829aa41a352b896c737a9a8
thabatafrs <thabatafreitas10@gmail.com>
2024-02-26 10:27:15 -0300

    atualizações

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ function entrar() {
   document.getElementById("entrar");
   location.href = "hub.html";
 }
+
+function iniciarProcesso() {
+  document.getElementById("hub");
+  location.href = "id_eleitor.html";
+}
+
+function hubSair() {
+  document.getElementById("hubSair");
+  location.href = "index.html";
+}
+
+function entrarUrna() {
+  document.getElementById("id_eleitor");
+  location.href = "id_eleitor.html";
+}
