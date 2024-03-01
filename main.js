063eeb782e0e4aa383b737916409ff9369d014df
ewanuel <emanuelmedeirosrosa@outlook.com>
2024-03-01 17:02:36 -0300

    v0.2

--- main.js
+++ main.js
@@ -19,6 +19,22 @@ function entrarContagem() {
 }
 
 function entrarUrna() {
-  document.getElementById("id_eleitor");
-  location.href = "id_eleitor.html";
+  const newWindow = window.open("voto.html", "_blank", "width=1360,height=768");
+  if (newWindow) {
+      const left = 1360; 
+      const top =2341; 
+      newWindow.moveTo(left, top);
+  } else {
+      alert("Pop-up bloqueado. Por favor, habilite os pop-ups para abrir a página em uma nova janela.");
+  }
+}
+
+function cancelar() {
+  document.getElementById("cancela");
+  location.href = "voto.html"
+}
+
+function confirma() {
+  document.getElementById("confirma");
+  location.href = "FIM.html"
 }
