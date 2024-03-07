16d17c13f8d6cceefcc42b27bddc7f86deec40c9
Thabata Freitas <139194194+thabatafrs@users.noreply.github.com>
2024-03-07 13:05:36 -0300

    adicionando botão de cadastro

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ function entrar() {
   location.href = "hub.html";
 }
 
+function cadastrar() {
+  document.getElementById("cadastrar");
+  location.href = "cadastroAdmin.html";
+}
+
 function iniciarProcesso() {
   document.getElementById("hub");
   location.href = "id_eleitor.html";
@@ -13,6 +18,11 @@ function hubSair() {
   location.href = "index.html";
 }
 
+function entrarCadastrarChapas() {
+  document.getElementById("cadastrarChapas");
+  location.href = "cadastroChapas.html";
+}
+
 function entrarContagem() {
   document.getElementById("entrarContagem");
   location.href = "contagem.html";
@@ -21,20 +31,22 @@ function entrarContagem() {
 function entrarUrna() {
   const newWindow = window.open("voto.html", "_blank", "width=1360,height=768");
   if (newWindow) {
-      const left = 1360; 
-      const top =2341; 
-      newWindow.moveTo(left, top);
+    const left = 1360;
+    const top = 2341;
+    newWindow.moveTo(left, top);
   } else {
-      alert("Pop-up bloqueado. Por favor, habilite os pop-ups para abrir a página em uma nova janela.");
+    alert(
+      "Pop-up bloqueado. Por favor, habilite os pop-ups para abrir a página em uma nova janela."
+    );
   }
 }
 
 function cancelar() {
   document.getElementById("cancela");
-  location.href = "voto.html"
+  location.href = "voto.html";
 }
 
 function confirma() {
   document.getElementById("confirma");
-  location.href = "FIM.html"
+  location.href = "FIM.html";
 }
